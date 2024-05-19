7bbc119f909bca6f19258cb94779e3520982bc00
Ryan Tam <ryantam626@gmail.com>
2024-05-19 15:16:42 +0100

    feat: Window object

---
+++ mado-py/mado/window.py
@@ -0,0 +1,12 @@
+﻿import dataclasses
+import typing
+
+from mado.config import DEFAULT_ORIGIN
+from mado.types_ import WINDOW_HANDLE
+
+
+@dataclasses.dataclass
+class Window:
+    hwnd: WINDOW_HANDLE
+    # start x and start y of the screen that this window was last active on
+    origin: typing.Optional[bool] = dataclasses.field(init=False, default=DEFAULT_ORIGIN, compare=False)
