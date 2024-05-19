8258dd1f48a40de109ba5ea4388e6b1cffdb73b8
Ryan Tam <ryantam626@gmail.com>
2024-05-19 15:14:04 +0100

    feat: Commands skaffold

---
+++ mado-py/mado/types_.py
@@ -0,0 +1,7 @@
+﻿from typing import NewType, Tuple
+
+SCREEN_ID = NewType("SCREEN_ID", str)
+VIRTUAL_DESKTOP_ID = NewType("VIRTUAL_DESKTOP_ID", int)
+WINDOW_HANDLE = NewType("WINDOW_HANDLE", int)
+MONITOR_HANDLE = NewType("MONITOR_HANDLE", int)
+COORDINATES_ISH = Tuple[int, int, int, int] | Tuple[int, int]
---
+++ mado-py/mado/window_manager/commands.py
@@ -0,0 +1,58 @@
+﻿import dataclasses
+import enum
+
+from mado.types_ import SCREEN_ID
+
+
+@dataclasses.dataclass
+class WindowManagerCommand:
+    pass
+
+
+@dataclasses.dataclass
+class FocusVirtualDesktop(WindowManagerCommand):
+    virtual_desktop_id: int
+
+
+class StateDump(WindowManagerCommand):
+    """Dumping internal state of the window manager"""
+
+
+@dataclasses.dataclass
+class CycleFocusedWindow(WindowManagerCommand):
+    """Cycle focused window in the same workspace."""
+
+    class Direction(enum.Enum):
+        forward = enum.auto()
+        backward = enum.auto()
+
+    direction: Direction
+
+
+class Minimise(WindowManagerCommand):
+    """Minimise the currently focused window."""
+
+
+class ToggleMaximise(WindowManagerCommand):
+    """Maximise the currently focused window."""
+
+
+@dataclasses.dataclass
+class MoveToScreen(WindowManagerCommand):
+    """Move currently focused window to specified screen."""
+
+    screen_id: SCREEN_ID
+
+
+@dataclasses.dataclass
+class SendToScreen(WindowManagerCommand):
+    """Move currently focused window to specified screen."""
+
+    screen_id: SCREEN_ID
+
+
+@dataclasses.dataclass
+class FocusScreen(WindowManagerCommand):
+    """Focus specified screen."""
+
+    screen_id: SCREEN_ID
