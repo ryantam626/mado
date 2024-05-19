268beb4a88a3345dbba5088d5edcd494b663c1fe
Ryan Tam <ryantam626@gmail.com>
2024-05-19 15:21:04 +0100

    feat: WM skaffold

---
+++ mado-py/mado/window_manager/__init__.py
@@ -0,0 +1,44 @@
+﻿import queue
+
+from loguru import logger
+
+from mado.keyboard_manager import KeyboardManager
+from mado.win_event_listener import WinEventHookListener
+from mado.window_manager import commands
+from mado.window_manager import events
+from mado.window_manager.state import Screen, WindowManagerState
+
+
+class WindowManager:
+
+    def __init__(self) -> None:
+        self.event_queue = queue.Queue()
+        self.win_event_listener = WinEventHookListener(event_queue=self.event_queue)
+        self.keyboard_manager = KeyboardManager(event_queue=self.event_queue)
+
+        self.state = WindowManagerState.new()
+        self.should_not_manage_next_focus = False
+
+    def run(self) -> None:
+        logger.info("Starting Mado WindowManager...")
+        self.win_event_listener.start()
+        self.keyboard_manager.start()
+        self.event_processing_loop()
+
+    def event_processing_loop(self) -> None:
+        logger.info("Starting event processing loop")
+        while True:
+            event = self.event_queue.get()
+
+            if isinstance(event, events.WindowManagerEvent):
+                self.handle_window_manager_event(event)
+            elif isinstance(event, commands.WindowManagerCommand):
+                self.handle_window_manager_command(event)
+            else:
+                pass
+
+    def handle_window_manager_event(self, event: events.WindowManagerEvent) -> None:
+        logger.debug("Handling window manager event, {} (State: {})", event, self)
+
+    def handle_window_manager_command(self, event: commands.WindowManagerCommand) -> None:
+        logger.debug("Handling window manager command, {} (State: {})", event, self)
