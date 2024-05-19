9c4abe7961a8d5aad8001a29819f8638a8b7e4a1
Ryan Tam <ryantam626@gmail.com>
2024-05-19 15:16:17 +0100

    feat: Init config

---
+++ mado-py/mado/config.py
@@ -0,0 +1,24 @@
+﻿from mado.types_ import SCREEN_ID, VIRTUAL_DESKTOP_ID
+
+VIRTUAL_DESKTOP_IDS = [
+    VIRTUAL_DESKTOP_ID(1),  # Editor
+    VIRTUAL_DESKTOP_ID(4),  # Music
+    VIRTUAL_DESKTOP_ID(2),  # Terminal
+    VIRTUAL_DESKTOP_ID(3),  # Browser
+    VIRTUAL_DESKTOP_ID(5),  # Misc
+]
+
+SCREEN_IDS = [
+    SCREEN_ID("LEFT"),
+    SCREEN_ID("RIGHT"),
+]
+INIT_FOCUSED_SCREEN_ID = SCREEN_IDS[1]
+
+IGNORED_WINDOW_TITLES = {
+    "Task Switching",
+    "Chrome Legacy Window",
+}
+
+MOUSE_FOLLOWS_FOCUS = True
+
+DEFAULT_ORIGIN = (0, 0)
