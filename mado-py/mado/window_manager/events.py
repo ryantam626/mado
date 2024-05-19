5541168f872d7009635df85e409ebd414f50a226
Ryan Tam <ryantam626@gmail.com>
2024-05-19 15:20:38 +0100

    feat: Events skaffold

---
+++ mado-py/mado/window_manager/events.py
@@ -0,0 +1,54 @@
+﻿import dataclasses
+import typing
+
+from mado.window import Window
+
+
+@dataclasses.dataclass
+class WindowManagerEvent:
+    win_event: typing.Optional[int]
+    window: Window
+
+
+class Destroy(WindowManagerEvent):
+    pass
+
+
+class FocusChange(WindowManagerEvent):
+    pass
+
+
+class Hide(WindowManagerEvent):
+    pass
+
+
+class Cloak(WindowManagerEvent):
+    pass
+
+
+class Minimise(WindowManagerEvent):
+    pass
+
+
+class Show(WindowManagerEvent):
+    pass
+
+
+class Uncloak(WindowManagerEvent):
+    pass
+
+
+class MoveResizeStart(WindowManagerEvent):
+    pass
+
+
+class MoveResizeEnd(WindowManagerEvent):
+    pass
+
+
+class MouseCapture(WindowManagerEvent):
+    pass
+
+
+class Moved(WindowManagerEvent):
+    pass
