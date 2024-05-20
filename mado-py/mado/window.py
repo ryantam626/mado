7e4ba5b303b0c42d9c2ac4e18bfc0ae9cacc69e1
Ryan Tam <ryantam626@gmail.com>
2024-05-20 11:46:12 +0100

    chore: Fix wrong type

--- mado-py/mado/window.py
+++ mado-py/mado/window.py
@@ -14,4 +14,6 @@ class Window:
     # screen information which we can attach in internal logic/states, will not be used to compare.
     screen: typing.Optional["Screen"] = dataclasses.field(init=False, default=None, repr=False, compare=False)
     # start x and start y of the screen that this window was last active on
-    origin: typing.Optional[bool] = dataclasses.field(init=False, default=DEFAULT_ORIGIN, compare=False)
+    origin: typing.Optional[typing.Tuple[int, int]] = dataclasses.field(
+        init=False, default=DEFAULT_ORIGIN, compare=False
+    )
