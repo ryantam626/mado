feab62d79d30728c3079cc8e272c52e346489251
Ryan Tam <ryantam626@gmail.com>
2024-05-20 14:04:45 +0100

    chore: Info or above log only

--- mado-py/mado/main.py
+++ mado-py/mado/main.py
@@ -1,7 +1,14 @@
-﻿from mado.window_manager import WindowManager
+﻿import sys
+
+from loguru import logger
+
+from mado.window_manager import WindowManager
 
 
 def run():
+    logger.remove()
+    logger.add(sys.stderr, level="INFO")
+
     try:
         WindowManager().run()
     except KeyboardInterrupt:
